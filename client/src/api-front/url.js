c5cec640de39acb3b9ef4f46f85c23b98426a577
Toto <toto_lp77@hotmail.com>
2021-04-18 09:34:25 -0300

    url de crossref

--- client/src/api-front/url.js
+++ client/src/api-front/url.js
@@ -1,4 +1,4 @@
 //la parte de "works" implica buscar por query en este caso, se podria filtar si se conoce issn pero no implementado
 
-export const url = `https://api.crossref.org/works`;
+export const api = `http://localhost:3001/api`;
 
