ef114c1989a73c0405d513c65f613d1c1e7390ef
Toto <toto_lp77@hotmail.com>
2021-04-18 09:35:19 -0300

    rutas para search a la api en backend

--- server/index.js
+++ server/index.js
@@ -1,15 +1,37 @@
 const path = require('path');
 const express = require("express");
+const fetch = require("node-fetch");
+const e = require('express');
+const url = `https://api.crossref.org/works`;
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-
 app.use(express.static(path.resolve(__dirname, '../client/build')));
-
+// app.use(express.json({
+//   type: "*/*" // optional, only if you want to be sure that everything is parset as JSON. Wouldn't reccomand
+// }));
+app.use(express.urlencoded());
 app.get("/api", (req, res) => {
+  console.log('En /api');
   res.json({ message: "Hello from server!" });
+});
+
+app.post("/api/getContents", async (req, res) => {
+  console.log('Reqs desde el front al back:');
+  const data  = await fetch(url + `?query=${req.body.query}&sort=score&rows=1`);
+  if (data.ok){
+    console.log('Request successful');
+    const jsonData = await data.json();
+    console.log(jsonData.message.items[0]);
+    res.send(jsonData.message.items[0]);;
+  } else {
+    console.log('Request failed', res.ok);
+    res.send( { error: 'Something went wrong' });
+  }
+  //TODO: PARSEAR ESTA RESPUESTA CUANDO NO SEA 503
+  console.log('Data returned');
 });
 
 app.get('*', (req, res) => {
