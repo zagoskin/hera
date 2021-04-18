caa012a4c77f222fbddb7de93eb219680748df60
Toto <toto_lp77@hotmail.com>
2021-04-18 09:33:52 -0300

    Search a traves de api en backend

--- client/src/api-front/search.js
+++ client/src/api-front/search.js
@@ -1,19 +1,28 @@
-import { url } from "./url";
+import { api } from './url';
 
 export const getContentList = async (query) => {
   try {
-    const res = await fetch(url + `?query=${query}&sort=score&rows=1`);
-    const contents =  await res.json();
-
-    const doi = contents.message.items[0].DOI;
-    console.log(doi);
-    const redalycData = await fetch(`http://148.215.1.70/redalyc/oai?verb=GetRecord&identifier=oai:redalyc.org:10201906&metadataPrefix=oai_dc`, {
-      credentials: 'include'
+    const res = await fetch(`/api/getContents`, {
+      method: 'POST',
+      body: new URLSearchParams({
+        'query': query
+      }),
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      }
     });
-    console.log('redalycData');
-    console.log(redalycData);
+    console.log('Contents de la API en front:')
+    const contents = await res.json();
+    console.log(contents);
+    // const doi = contents.message.items[0].DOI;
+    // console.log(doi);
+    // const redalycData = await fetch(`http://148.215.1.70/redalyc/oai?verb=GetRecord&identifier=oai:redalyc.org:10201906&metadataPrefix=oai_dc`, {
+    //   credentials: 'include'
+    // });
+    // console.log('redalycData');
+    // console.log(redalycData);
 
-    return contents.message.items;
+     return [contents];
   } catch(e) {
     console.error(e);
   }
