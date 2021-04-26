88fab9e1b5799092077f8160df4dc28518b3b320
Toto <toto_lp77@hotmail.com>
2021-04-26 15:10:19 -0300

    Academic card parsea específico de crossref TODO de DOAJ y Microsoft

--- client/src/components/AcademicCard/index.jsx
+++ client/src/components/AcademicCard/index.jsx
@@ -1,33 +1,38 @@
 import React from 'react';
 import './card.css';
+import crossrefLogo from '../../images/crossrefLogo.png'
 
 export default function AcademicCard({content}){
   console.log('En Academic Card');
   console.log(content);
   return (
     <div className="card">
-      <div className="card--score">
+      <div className="card--scoreCrossref">
         <img className="card--score--image"
-          src='https://pbs.twimg.com/profile_images/1202973071604367366/72Ft7ljG.jpg'
+          src={crossrefLogo}
           alt='academic_image'
         />
-        <h3 className="card--score--text">Crossref score: {content.score.toFixed(2)}</h3>
+        <div className="card--score--text">
+          Referencias académicas en {content.crossref["reference-count"]} artículos
+          <p>Información provista por Crossref</p>
+        </div>
       </div>
       <div className="card--metrics">
         <br/>
       </div>
       <div className="card--content">
-        <h1 className="card--title">{content.title}</h1>
-        {content.author ? <p>
-          <em>Authors: {content.author.map((author,index) => (author.name? <span key={index}> {author.name} </span> :
+        <h1 className="card--title">{content.crossref.title}</h1>
+        <p><a href={content.crossref.URL}>Click aquí para ir al artículo</a></p>
+        {content.crossref.author ? <p>
+          <em>Authors: {content.crossref.author.map((author,index) => (author.name? <span key={index}> {author.name} </span> :
             <span key={index}> 
                {author.family} {author.given}. 
             </span>         
           ))}</em> 
         </p> : null 
         }
-        {content.abstract ? <p className="card--desc">
-            ABSTRACT: {content.abstract.replace(/(<([^>]+)>)/ig, '')}
+        {content.crossref.abstract ? <p className="card--desc">
+            ABSTRACT: {content.crossref.abstract.replace(/(<([^>]+)>)/ig, '')}
           </p> : null
         } 
       </div>
