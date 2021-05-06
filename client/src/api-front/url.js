72e88e10fdb356b5c621593aaae0f09663f4feb5
Toto <toto_lp77@hotmail.com>
2021-05-06 16:55:00 -0300

    js de urls, set y get dependiendo si es DOI o ISSN

--- client/src/api-front/url.js
+++ client/src/api-front/url.js
@@ -1,4 +1,34 @@
 //la parte de "works" implica buscar por query en este caso, se podria filtar si se conoce issn pero no implementado
 
 export const api = `http://localhost:3001/api`;
+var urlCrossRef;
+var urlDoaj;
+var urlMicrosoft;
 
+export const setURLsByDOI = (DOI) => {
+  urlCrossRef = `https://api.crossref.org/works/${DOI}`;
+  urlDoaj = `https://doaj.org/api/v2/search/articles/doi:${DOI}`;
+  const doiUP = DOI.toUpperCase();
+  urlMicrosoft = `https://api.labs.cognitive.microsoft.com/academic/v1.0/evaluate?expr=DOI=='${doiUP}'&attributes=DOI,Ti,CC,ECC,AA.AuN,AA.AuId,AA.S,F.DFN,J.JId`;
+  //otras URLs
+}
+
+export const setURLsByISSN = (ISSN) => {
+  urlCrossRef = `https://api.crossref.org/journals/${ISSN}`;
+  urlDoaj = `https://doaj.org/api/v2/search/journals/issn:${ISSN}`;
+  //Microsoft no busca por ISSN así que esto devuelve un arreglo vacío en el campo entities
+  urlMicrosoft = `https://api.labs.cognitive.microsoft.com/academic/v1.0/evaluate?expr=DOI=='${ISSN}'&attributes=DOI,Ti,CC,ECC,AA.AuN,AA.AuId,AA.S,F.DFN`;
+  //otras URLs
+}
+
+export const getURLCrossref = () => {
+  return urlCrossRef;
+}
+
+export const getURLDoaj = () => {
+  return urlDoaj;
+}
+
+export const getURLMicrosoft = () => {
+  return urlMicrosoft;
+}
