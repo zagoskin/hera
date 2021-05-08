6f22251b09bc44cd9d352a539581dfe27705a763
Toto <toto_lp77@hotmail.com>
2021-05-08 11:16:40 -0300

    Parseo con nuevos campos en el objeto de contents

--- client/src/components/AcademicCard/index.jsx
+++ client/src/components/AcademicCard/index.jsx
@@ -12,10 +12,13 @@ export default function AcademicCard({content}){
           src={crossrefLogo}
           alt='crossref_image'
         />
-        <div className="card--crossref--text">
+        {content.crossref.error ? <div className="card--doaj--text--warning">
+          No hallado en Crossref 
+        </div>: <div className="card--crossref--text">
           Referencias académicas en {content.crossref["reference-count"]} artículos
           <p>Información provista por Crossref</p>
-        </div>
+        </div> }
+        
       </div>
 
       <div className="card--doaj">
@@ -23,18 +26,20 @@ export default function AcademicCard({content}){
           src={doajLogo}
           alt='doaj_image'
         />
-        {content.doaj.total === 0 ? 
+        {content.doaj === 0 ? 
         <div className="card--doaj--text--warning">
           No incluido en DOAJ
         </div> : [
-        <div className="card--doaj--text--success" key={content.doaj.total}>
+        <div className="card--doaj--text--success" key={content.doaj}>
           Indexado por DOAJ
         </div>,
-          (content.doaj.results[0].admin.seal ? 
-            <a href='https://doaj.org/apply/seal/' key={content.doaj.results[0].id}>
+          (content.doaj.admin ? 
+            content.doaj.admin.seal ?
+            <a href='https://doaj.org/apply/seal/' key={content.doaj.id}>
               
               <span className='card--doaj--seal'>☑ DOAJ Seal</span>
             </a>
+             : null 
           : null
           )]
         }
@@ -45,18 +50,18 @@ export default function AcademicCard({content}){
           src={microsoftLogo}
           alt='microsoft_image'
         />
-        {content.microsoft.entities.length > 0 ? 
+        {content.microsoft ? 
         <div className="card--microsoft--text">
           <div className="card--microsoft--text--cc">
-            Citas actuales {content.microsoft.entities[0].CC} 
+            Citas actuales {content.microsoft.CC} 
           </div>
           <div className="card--microsoft--text--ecc">
-            Citas estimadas {content.microsoft.entities[0].ECC} 
+            Citas estimadas {content.microsoft.ECC} 
           </div>
           <br/>
           <div className="card--microsoft--fos">
             <div className="card--microsoft--fos--title">Campos de Estudio</div>
-            {content.microsoft.entities[0].F.map((field,index) => 
+            {content.microsoft.F.map((field,index) => 
               <div className="card--microsoft--fos--badge" key={index}>{field.DFN}</div>   
             )}
           </div>
@@ -69,13 +74,16 @@ export default function AcademicCard({content}){
       </div>
 
       <div className="card--content">
-        <h1 className="card--title">{content.crossref.title}</h1>
-        <p><a href={content.crossref.URL}>Click aquí para ir al artículo</a></p>
-        {content.crossref.author ? <p>
-          <em>Authors: {content.crossref.author.map((author,index) => (author.name? <span key={index}> {author.name} </span> :
-            <span key={index}> 
-               {author.family} {author.given}. 
-            </span>         
+        <h1 className="card--title">{content.title}</h1>
+        <p><a href={content.URL}>Click aquí para ir al artículo</a></p>
+        {content.authors ? <p>
+          <em>Authors: {content.authors.map((author,index) => 
+          (author.name ? 
+            <span key={index}> {author.name}. </span> :
+          author.family ?
+            <span key={index}> {author.family} {author.given}. </span> :
+          author.AuN ?
+            <span key={index}> {author.AuN} . </span> : null
           ))}</em> 
         </p> : null 
         }
