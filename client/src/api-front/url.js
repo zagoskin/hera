5d7b797a5e636a1c44721a988ad231c6083c2813
Toto <toto_lp77@hotmail.com>
2021-05-08 11:16:08 -0300

    Agregados atributos de URL microsoft para DOI con abstract y titulo

--- client/src/api-front/url.js
+++ client/src/api-front/url.js
@@ -9,7 +9,7 @@ export const setURLsByDOI = (DOI) => {
   urlCrossRef = `https://api.crossref.org/works/${DOI}`;
   urlDoaj = `https://doaj.org/api/v2/search/articles/doi:${DOI}`;
   const doiUP = DOI.toUpperCase();
-  urlMicrosoft = `https://api.labs.cognitive.microsoft.com/academic/v1.0/evaluate?expr=DOI=='${doiUP}'&attributes=DOI,Ti,CC,ECC,AA.AuN,AA.AuId,AA.S,F.DFN,J.JId`;
+  urlMicrosoft = `https://api.labs.cognitive.microsoft.com/academic/v1.0/evaluate?expr=DOI=='${doiUP}'&attributes=DOI,Ti,CC,ECC,AA.AuN,AA.AuId,AA.S,F.DFN,AW,DN`;
   //otras URLs
 }
 
@@ -17,7 +17,7 @@ export const setURLsByISSN = (ISSN) => {
   urlCrossRef = `https://api.crossref.org/journals/${ISSN}`;
   urlDoaj = `https://doaj.org/api/v2/search/journals/issn:${ISSN}`;
   //Microsoft no busca por ISSN así que esto devuelve un arreglo vacío en el campo entities
-  urlMicrosoft = `https://api.labs.cognitive.microsoft.com/academic/v1.0/evaluate?expr=DOI=='${ISSN}'&attributes=DOI,Ti,CC,ECC,AA.AuN,AA.AuId,AA.S,F.DFN`;
+  urlMicrosoft = `https://api.labs.cognitive.microsoft.com/academic/v1.0/evaluate?expr=DOI=='${ISSN}'&attributes=DOI,Ti,CC,ECC,AA.AuN,AA.AuId,AA.S,F.DFN,AW,DN`;
   //otras URLs
 }
 
