ccddaf21f746e0e06b46bbc97e9ed6801eea7ef2
Toto <toto_lp77@hotmail.com>
2021-05-13 16:36:24 -0300

    ScopusGraph no implementado todavia

---
+++ client/src/components/ScopusGraph/index.jsx
@@ -0,0 +1,20 @@
+// import React, { useState, useEffect } from 'react';
+// import { Helmet } from 'react-helmet';
+// import dimensionsLogo from '../../images/dimensionsLogo.png';
+// import useScript from '../../hooks/useScript';
+
+
+export default function ScopusGraph(){
+  // useScript('https://badge.dimensions.ai/badge.js'); 
+  //const [scriptUrl, setScriptUrl] = useState('');
+  // useEffect(() => {
+  //   setDoi(DOI);
+  //   if (!loaded) return
+  // }, [DOI, loaded, error]);
+
+  return (
+    <div className="card--crossref">
+        
+      </div>
+  )
+}
