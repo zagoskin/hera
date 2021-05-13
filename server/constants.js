88efd38d7687d18fea2ac988ff325c20b8e66090
Toto <toto_lp77@hotmail.com>
2021-05-13 16:37:47 -0300

    Constantes de keys movidas a archivo separado

---
+++ server/constants.js
@@ -0,0 +1,9 @@
+module.exports = Object.freeze({
+  MICROSOFT_KEY: 'e49b412041094951957423bde5ad243a',
+  SCOPUS_KEY: 'd96a52800bd20431daec3f02f73557f8'
+});
+
+
+// const urlCrossRef = `https://api.crossref.org/works`;
+// const urlDoaj = `https://doaj.org/api/v2/search/articles/`;
+// const urlMicrosoft = `https://api.labs.cognitive.microsoft.com/academic/v1.0/evaluate`;
