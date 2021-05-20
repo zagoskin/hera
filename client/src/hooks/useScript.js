b5d5b49ef2652d36f5d387c55b7875057cdfe90c
Toto <toto_lp77@hotmail.com>
2021-05-20 13:12:37 -0300

    Refactoring e import de script altmetric

--- client/src/hooks/useScript.js
+++ client/src/hooks/useScript.js
@@ -1,10 +1,10 @@
 import { useEffect } from 'react';
 
-const useScript = (url) => {
+export const useDimensions = (url) => {
   useEffect(() => {
     let script = document.createElement('script');
     script.setAttribute('id', 'myScript');
-    script.src = 'https://badge.dimensions.ai/badge.js';
+    script.src = url;
     script.async = true;
     
     document.getElementsByTagName('body')[0].appendChild(script);
@@ -19,4 +19,18 @@ const useScript = (url) => {
   }, [url]);
 };
 
-export default useScript;
+export const useAltmetric = (url) => {
+  useEffect(() => {
+    let script = document.createElement('script');
+    script.setAttribute('id', 'altmetricScript');
+    script.src = url;
+    script.type = 'text/javascript';
+    
+    document.getElementsByTagName('body')[0].appendChild(script);
+
+    return () => {
+      let myScript = document.getElementById('altmetricScript');
+      myScript.remove();
+    }
+  }, [url]);
+}
