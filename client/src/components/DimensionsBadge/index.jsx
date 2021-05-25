67b07ee23a062ae35ce014f9370c0e8ea7951d75
Toto <toto_lp77@hotmail.com>
2021-05-25 11:28:00 -0300

    Imports viejos removidos

--- client/src/components/DimensionsBadge/index.jsx
+++ client/src/components/DimensionsBadge/index.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-//import { Helmet } from 'react-helmet';
 import { useDimensions } from '../../hooks/useScript';
 import dimensionsLogo from '../../images/dimensionsLogo.png';
 
