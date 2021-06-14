631c50bfb49caefbb0da8e820e1ade965ee7fc1a
Toto <toto_lp77@hotmail.com>
2021-06-14 13:45:43 -0300

    RegExp ISSN modificado

--- client/src/helpers/regex.js
+++ client/src/helpers/regex.js
@@ -1,5 +1,5 @@
 export const validDOI = new RegExp('^10.[0-9]{4}/');
 
-export const validISSN = new RegExp('^[0-9]{4}-[0-9]{4}');
+export const validISSN = new RegExp('^[0-9]{4}-[0-9]{3}[0-9xX]$');
 
 //^10\.+[0-9]{4}+\/.i
