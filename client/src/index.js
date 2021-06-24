fecc839458d3ca1eb5b83f3e12d1a925e46b898c
Toto <toto_lp77@hotmail.com>
2021-06-24 12:08:38 -0300

    router y link, no se usa todavia pero puede servir

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './index.css';
 import Search from './components/Search/index';
 
-class Main extends React.Component{
-  render(){
-    return(
-      <div className="container">
-        <h1 className="title">Filtro Académico</h1>
-        <Search/>
-      </div>
-      
+class Main extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <nav></nav>
+          <Link to="/"><h1 className="title">Filtro Académico</h1></Link>
+          <Switch>
+            <Route path="/about">
+              <div>
+                Un about
+              </div>
+            </Route>
+            <Route path="/">
+              <Search />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+
     );
   }
 }
