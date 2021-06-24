92ebd6f5b5e37bdedeeaf5002c25b96f031e052c
Toto <toto_lp77@hotmail.com>
2021-06-24 12:07:01 -0300

    Incorporacion de llamados a api para data de redib y semantic

--- client/src/api-front/dataBuilder.js
+++ client/src/api-front/dataBuilder.js
@@ -1,25 +1,30 @@
-import { getContentsCrossref, getContentsDoaj, getContentsMicrosoft, getContentsScopus, getContentsDimensions, getContentsAltmetric, getContentsScimago } from './search'
-import { getURLCrossref, getURLDoaj, getURLMicrosoft, getURLScopus, getURLDimensions, getURLAltmetric, setURLsByDOI, setURLsByISSN, getURLScimago } from './url';
+import { getContentsCrossref, getContentsDoaj, getContentsMicrosoft, getContentsScopus, getContentsDimensions, getContentsAltmetric, getContentsScimago, getContentsSemantic, getContentsRedib } from './search'
+import { getURLCrossref, getURLDoaj, getURLMicrosoft, getURLScopus, getURLDimensions, getURLAltmetric, setURLsByDOI, setURLsByISSN, getURLScimago, getURLSemantic, getURLRedib } from './url';
 
-export const getDataByQuery = async (query,criteria) => { 
-  
+export const getDataByQuery = async (query, criteria) => {
+
   let microsoftData = [];
   let scopusData;
   let dimensionsData;
   let altmetricData;
   let scimagoData;
-  if (criteria === "DOI"){
+  let semanticData;
+  let redibData;
+
+  if (criteria === "DOI") {
     setURLsByDOI(query);
     microsoftData = await getContentsMicrosoft(getURLMicrosoft());
     dimensionsData = await getContentsDimensions(getURLDimensions());
     altmetricData = await getContentsAltmetric(getURLAltmetric());
-  }else {
-    if (criteria === "ISSN"){
+    semanticData = await getContentsSemantic(getURLSemantic());
+  } else {
+    if (criteria === "ISSN") {
       setURLsByISSN(query);
       scopusData = await getContentsScopus(getURLScopus());
+      redibData = await getContentsRedib(getURLRedib());
     }
   }
-  const crossrefData = await getContentsCrossref(getURLCrossref()); 
+  const crossrefData = await getContentsCrossref(getURLCrossref());
   const doajData = await getContentsDoaj(getURLDoaj());
 
   let res = {
@@ -29,16 +34,18 @@ export const getDataByQuery = async (query,criteria) => {
     scopus: scopusData ?? null,
     dimensions: dimensionsData,
     altmetric: altmetricData,
-    abstract: 
-      crossrefData.abstract ? 
-        !(Array.isArray(crossrefData.abstract)) ? 
-          crossrefData.abstract 
-        : (doajData.total > 0) ? 
-          doajData.results[0].bibjson.abstract 
-        : microsoftData.length > 0 ? 
-          microsoftData[0].AW 
-        : '' 
-      : '',
+    semantic: semanticData,
+    redib: redibData,
+    abstract:
+      crossrefData.abstract ?
+        !(Array.isArray(crossrefData.abstract)) ?
+          crossrefData.abstract
+          : (doajData.total > 0) ?
+            doajData.results[0].bibjson.abstract
+            : microsoftData.length > 0 ?
+              microsoftData[0].AW
+              : ''
+        : '',
     title: crossrefData.title ? crossrefData.title : doajData.total > 0 ? doajData.results[0].bibjson.title : microsoftData.length > 0 ? microsoftData[0].DN : '',
     URL: criteria === 'DOI' ? `https://dx.doi.org/${query}` : doajData.total > 0 ? doajData.results[0].bibjson.ref.journal : `https://portal.issn.org/resource/ISSN/${query}`,
     authors: crossrefData.author ? crossrefData.author : doajData.total > 0 ? doajData.results[0].bibjson.author : microsoftData.length > 0 ? microsoftData[0].AA : undefined,
@@ -48,11 +55,11 @@ export const getDataByQuery = async (query,criteria) => {
     },
   }
 
-  if (criteria === "ISSN"){
-    scimagoData = await getContentsScimago(getURLScimago(),res.title);
+  if (criteria === "ISSN") {
+    scimagoData = await getContentsScimago(getURLScimago(), res.title);
   }
 
-  res = {...res, scimago: scimagoData}
+  res = { ...res, scimago: scimagoData }
 
   return res;
 }
