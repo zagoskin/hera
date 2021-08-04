28ea26e8d0f067d0f8cdafcf34c254798719cddb
Toto <toto_lp77@hotmail.com>
2021-08-04 13:46:09 -0300

    Posible fix para DOIs con más de 4 números en prefijo e incluso posteriormente seccionados con .

--- client/src/helpers/regex.js
+++ client/src/helpers/regex.js
@@ -1,4 +1,9 @@
-export const validDOI = new RegExp('^10.[0-9]{4}/');
+//Actualmente este regexp intenta capturar el prefijo posible de 10.NNNN.NN sugerido por https://www.doi.org/doi_handbook/2_Numbering.html#2.2.2
+export const validDOI = new RegExp(/^10.\d{4,9}(\.\d+)?\/[-._;()/:A-Z0-9]+$/, 'i');
+
+
+//crossref regexp DOI^10.\d{4,9}/[-._;()/:A-Z0-9]+$/
+//old DOI ^10.[0-9]{4}/
 
 export const validISSN = new RegExp('^[0-9]{4}-[0-9]{3}[0-9xX]$');
 
