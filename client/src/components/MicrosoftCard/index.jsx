ffc03f897cd846f76d6e927ae3ff2aef15228e40
Toto <toto_lp77@hotmail.com>
2021-08-26 11:10:20 -0300

    Cambio en algunos textos. Microsoft card ocupa siempre menos

--- client/src/components/MicrosoftCard/index.jsx
+++ client/src/components/MicrosoftCard/index.jsx
@@ -6,35 +6,39 @@ export default function MicrosoftCard({identifier, content}){
 
   return (
     <div className="card--microsoft">
-      <a href={content ? `https://academic.microsoft.com/paper/${content.Id}` 
-        : `https://academic.microsoft.com/home` }>
-        <img className="card--microsoft--image"
-          src={microsoftLogo}
-          alt='microsoft_image'
-        />
-      </a>
-      {content ? 
-      <div className="card--microsoft--text">
-        <div className="card--microsoft--text--cc">
-          Citas actuales {content.CC} 
-        </div>
-        <div className="card--microsoft--text--ecc">
-          Citas estimadas {content.ECC} 
-        </div>
-        <br/>
-        <div className="card--microsoft--fos">
-          <div className="card--microsoft--fos--title">Campos de Estudio</div>
-          {content.F.map((field,index) => 
-            <div className="card--microsoft--fos--badge" key={index}>{field.DFN}</div>   
-          )}
-        </div>
-      </div> 
-      : identifier.type === "DOI" ?
-      <div className="card--doaj--text--warning">
-        No hallado en Bing! 
+      <div className="card--image--container">
+        <a href={content ? `https://academic.microsoft.com/paper/${content.Id}` 
+          : `https://academic.microsoft.com/home` }>
+          <img className="card--microsoft--image"
+            src={microsoftLogo}
+            alt='microsoft_image'
+          />
+        </a>
       </div>
-      : null
-      }
+      <div className="card--data--container">
+        {content ? 
+        <div className="card--microsoft--text">
+          <div className="card--microsoft--text--cc">
+            Citas actuales: {content.CC} 
+          </div>
+          <div className="card--microsoft--text--cc">
+            Citas que se estima recibir el próximo año: {content.ECC} 
+          </div>
+          <br/>
+          <div className="card--microsoft--fos">
+            <div className="card--microsoft--fos--title">Campos de Estudio</div>
+            {content.F.map((field,index) => 
+              <div className="card--microsoft--fos--badge" key={index}>{field.DFN}</div>   
+            )}
+          </div>
+        </div> 
+        : identifier.type === "DOI" ?
+        <div className="card--doaj--text--warning">
+          No hallado en Bing! 
+        </div>
+        : null
+        }
+      </div>
     </div>
   )
 }
