446811415839a96731b0f0939d3459ef9310b3b6
Toto <toto_lp77@hotmail.com>
2021-08-26 11:07:55 -0300

    CrossrefCard es mas chico cuando analizo DOI

--- client/src/components/CrossrefCard/index.jsx
+++ client/src/components/CrossrefCard/index.jsx
@@ -5,7 +5,12 @@ import crossrefLogo from '../../images/crossrefLogo.png';
 export default function CrossrefCard({identifier, content}){
 
   return (
-    <div className="card--crossref">
+    <div className="card--crossref" style={identifier.type === "DOI" ? 
+      { width: "48%",
+        marginRight: "1%" } 
+        : {width: "100%"}
+      } >
+      <div className="card--image--container" style={identifier.type === "ISSN" ? {width: "17%" } : null}>
       <a href={ content.error ? `https://www.crossref.org/` 
         : `https://search.crossref.org/?from_ui=yes&q=${identifier.value}`}>
         <img className="card--crossref--image"
@@ -13,6 +18,8 @@ export default function CrossrefCard({identifier, content}){
           alt='crossref_image'
         />
       </a>
+      </div>
+      <div className="card--data--container">
       {content.error ? <div className="card--doaj--text--warning">
         No hallado en Crossref 
       </div>: 
@@ -28,6 +35,12 @@ export default function CrossrefCard({identifier, content}){
         <div className="card--crossref--info">
           {content.counts["current-dois"]} artículos publicados en esta revista
         </div>
+        <div className="card--crossref--info">
+          {(content.coverage["award-numbers-current"] * 100).toFixed(1)}% de los artículos en esta revista tienen algún award
+        </div>
+        <div className="card--crossref--info">
+          {(content.coverage["orcids-current"] * 100).toFixed(1)}% de los autores de la revista tienen un <a href='https://orcid.org/'>ORCID</a>
+        </div>
         <div className="card--crossref-breakdowns">
           <div className="card--microsoft--fos--title">Artículos por año</div>
           {content.breakdowns["dois-by-issued-year"].sort((a,b) => a[0] - b[0]).map((yearbydoi,index) => 
@@ -36,16 +49,11 @@ export default function CrossrefCard({identifier, content}){
             </div>   
           )}
         </div>
-        <div className="card--crossref--info">
-          {(content.coverage["award-numbers-current"] * 100).toFixed(1)}% de los artículos en esta revista tienen algún award
-        </div>
-        <div className="card--crossref--info">
-          {(content.coverage["orcids-current"] * 100).toFixed(1)}% de los artículos tienen un <a href='https://orcid.org/'>ORCID</a>
-        </div>
         <p>Información provista por Crossref</p>
       </div> 
       : null     
       }
+      </div>
     </div>
   )
 }
