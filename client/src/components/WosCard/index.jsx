145f9648400dc54ce95a46312fb56522e09aba70
Toto <toto_lp77@hotmail.com>
2021-08-26 11:15:50 -0300

    WosCard render de periodicidad

--- client/src/components/WosCard/index.jsx
+++ client/src/components/WosCard/index.jsx
@@ -4,6 +4,38 @@ import '../TinyPanel/tinycard.css';
 
 export default function WosCard({ISSN, content}){
 
+  const renderFrequencySwitch = (frequencyChar) => {
+    switch(frequencyChar) {
+      case 'A': 
+        return 'Anual';
+      case 'R':
+        return 'Artículo por artículo';
+      case 'B':
+        return 'Bimestral';
+      case 'C':
+        return 'Publicación continua';
+      case 'F':
+        return 'Quincenal';
+      case 'I':
+        return 'Irregular';
+      case 'M':
+        return 'Mensual';
+      case 'Q':
+        return 'Trimestral';
+      case 'H':
+        return 'Cada 6 meses';
+      case 'S':
+        return 'Dos veces al mes';
+      case 'T':
+        return 'Tres veces al año';
+      case 'W':
+        return 'Semanal';
+      default:
+        return '';
+    }
+
+  }
+
   return (
     <div className="card--wos">
       <div className="card--scimago--logo">
@@ -36,6 +68,7 @@ export default function WosCard({ISSN, content}){
             <div style={{fontWeight: "normal", width: '100%'}} className="card--tiny--info--data">EDITOR<br /><br /></div>
             <div style={{fontSize: "1.75rem",width: '100%'}} className="card--tiny--info--data">{content.journalProfiles[0].journalProfile.publisherName}</div>          
           </div>  
+          
           {content.journalProfiles[0].journalProfile.publicationLanguages ?
           <div style={{flex: '1 auto', width: '30%'}} className="card--scimago--info redib">
             <div style={{fontWeight: "normal", width: '100%'}} className="card--tiny--info--data">IDIOMAS<br /><br /></div>  
@@ -47,6 +80,7 @@ export default function WosCard({ISSN, content}){
           </div>      
           : null 
           }    
+
           {content.journalProfiles[0].journalProfile.country ?
           <div style={{flex: '1 auto', width: '30%'}} className="card--scimago--info redib">
             <div style={{fontWeight: "normal", padding: "1rem"}} className="card--tiny--info--data">PAÍS<br /><br /></div>
@@ -54,8 +88,19 @@ export default function WosCard({ISSN, content}){
           </div>    
           : null
           }
+
+          {content.journalProfiles[0].journalProfile.publicationFrequency ?
+          <div style={{flex: '1 auto', width: '30%'}} className="card--scimago--info redib">
+            <div style={{fontWeight: "normal", padding: "1rem"}} className="card--tiny--info--data">PERIODICIDAD<br /><br /></div>
+            <div style={{padding: "1rem"}} className="card--tiny--info--data">
+              {renderFrequencySwitch(content.journalProfiles[0].journalProfile.publicationFrequency)}              
+            </div>      
+          </div>    
+          : null
+          }
+
           { content.journalProfiles[0].journalProfile.categories.length > 0 ?
-          <div style={{flex: '1 auto', flexWrap: 'wrap', alignItems: 'stretch'}} className="card--scimago--info redib">
+          <div style={{flex: '1 auto', width: '60%', flexWrap: 'wrap', alignItems: 'stretch'}} className="card--scimago--info redib">
             <div style={{fontWeight: "normal", padding: "1rem"}} className="card--tiny--info--data">CATEGORÍAS<br /><br /></div>
             {content.journalProfiles[0].journalProfile.categories.map((category, index) => 
             <div key={`${category} ${index}`} style={{fontFamily: 'Roboto', width:'30%', padding: '0.5rem',backgroundColor: '#004aff', color: 'white'}} className="card--tiny--info--indicator">
