0fbb61b6b5cdf322c35b4a5e93168f29655b04d2
Toto <toto_lp77@hotmail.com>
2021-10-03 11:19:26 -0300

    Logica para parsear timeout o errores

--- client/src/api-front/searcher.js
+++ client/src/api-front/searcher.js
@@ -98,115 +98,127 @@ export const getContentsWos = async (url, issn) => {
 
 export const getContentsScimago = async (url, title) => {
   const res = await getHtml(url, `/api/getContentsHtml`, '');
-  const searchHtml = res.html;
 
-  let parser = new DOMParser();
-
-  const searchDOM = parser.parseFromString(searchHtml, 'text/html');
-  const errorP = searchDOM.body.querySelector(".journaldescription.colblock").querySelector("h2");
-
-  let contents;
-
-  if (errorP) { //busco si hay un resultado en la busqueda
-    contents = {
-      error: errorP.innerText
-    }
+  if (res.html.error){
+    return { error: res.html.error };
   } else {
-    const searchResults = searchDOM.body.querySelector(".search_results");
+      const searchHtml = res.html;
 
-    const firstTitle = searchResults.querySelector(".jrnlname").innerText;
-
-    if ((searchResults.children.length === 1) || (firstTitle.split(" ")[0] === title.split(" ")[0])) {
-      const anchorURL = searchResults.querySelector("a").href;
-
-      const journalURL = "https://www.scimagojr.com/" + anchorURL.replace(/^(?:\/\/|[^/]+)*\//, '');
-      //alert(journalURL);
-      const searchRes = await getHtml(journalURL, `/api/getContentsHtml`, '');
-      const journalHtml = searchRes.html;
-
-      const journalDOM = parser.parseFromString(journalHtml, 'text/html');
-      const hIndex = journalDOM.body.querySelector(".hindexnumber").innerText;
-      const embedString = journalDOM.getElementById("embed_code").value;
-      const journalData = journalDOM.body.querySelector(".journalgrid").querySelectorAll("div");
-      const country = journalData[0].querySelector("p").querySelector("a").innerText;
-      const coverage = journalData[6].querySelector("p").innerText;
-      const publisher = journalDOM.body.querySelector(".journalgrid").querySelectorAll("div")[2].querySelector("p").innerText;
-      contents = {
-        hIndex,
-        country,
-        coverage,
-        embedString,
-        journalURL,
-        publisher
+      let parser = new DOMParser();
+    
+      const searchDOM = parser.parseFromString(searchHtml, 'text/html');
+      const errorP = searchDOM.body.querySelector(".journaldescription.colblock").querySelector("h2");
+    
+      let contents;
+    
+      if (errorP) { //busco si hay un resultado en la busqueda
+        contents = {
+          error: errorP.innerText
+        }
+      } else {
+        const searchResults = searchDOM.body.querySelector(".search_results");
+    
+        const firstTitle = searchResults.querySelector(".jrnlname").innerText;
+    
+        if ((searchResults.children.length === 1) || (firstTitle.split(" ")[0] === title.split(" ")[0])) {
+          const anchorURL = searchResults.querySelector("a").href;
+    
+          const journalURL = "https://www.scimagojr.com/" + anchorURL.replace(/^(?:\/\/|[^/]+)*\//, '');
+          //alert(journalURL);
+          const searchRes = await getHtml(journalURL, `/api/getContentsHtml`, '');
+          const journalHtml = searchRes.html;
+    
+          const journalDOM = parser.parseFromString(journalHtml, 'text/html');
+          const hIndex = journalDOM.body.querySelector(".hindexnumber").innerText;
+          const embedString = journalDOM.getElementById("embed_code").value;
+          const journalData = journalDOM.body.querySelector(".journalgrid").querySelectorAll("div");
+          const country = journalData[0].querySelector("p").querySelector("a").innerText;
+          const coverage = journalData[6].querySelector("p").innerText;
+          const publisher = journalDOM.body.querySelector(".journalgrid").querySelectorAll("div")[2].querySelector("p").innerText;
+          contents = {
+            hIndex,
+            country,
+            coverage,
+            embedString,
+            journalURL,
+            publisher
+          }
+        } else {
+          contents = {
+            error: "Not found in list"
+          }
+        }
       }
-    } else {
-      contents = {
-        error: "Not found in list"
-      }
-    }
+    
+      // console.log('Resultados SCIMAGO a devolver:');
+      // console.log(contents);
+    
+      return contents;
   }
 
-  // console.log('Resultados SCIMAGO a devolver:');
-  // console.log(contents);
-
-  return contents;
 }
 
 export const getContentsRedib = async (url) => {
   const res = await getHtml(url, `/api/getContentsHtml`, '');
-
-  let parser = new DOMParser();
-
-  const resDOM = parser.parseFromString(res.html, 'text/html');
-  const errorP = resDOM.body.querySelector(".pull-left.help-block");
-  let contents;
-
-  if (errorP) {
-    contents = {
-      error: errorP.innerText,
-    }
+  console.log("Respuesta de getHtml redib");
+  console.log(res);
+  if (res.html.error) {
+    return { error: res.html.error };
   } else {
-    const journalTitle = resDOM.body.querySelector(".well-text-revista").querySelector("h1").innerText;
-    const journalURL = resDOM.body.querySelector(".table.table-striped").querySelector(".redibLink").href;
-    const redibURL = resDOM.body.querySelector(".dropdown").querySelector(".dropdown-menu").querySelector("a").href.split("?")[0];
-
-    const listaNodoIndicadores = resDOM.body.querySelector(".indicadores").querySelectorAll(".indicador1");
-    const arrayNodos = Array.from(listaNodoIndicadores);
-    const indicadores = arrayNodos.map((nodo) => nodo.innerText);
-
-    const listaAcredInter = resDOM.getElementsByClassName("acreditaciones")[0].querySelectorAll(".acreditacion1");
-    const arrayAcredInter = Array.from(listaAcredInter);
-    const acredInter = arrayAcredInter.map((nodo) => nodo.innerText);
-
-    const listaAcredNac = resDOM.getElementsByClassName("acreditaciones")[1].querySelectorAll(".acreditacion1");
-    const arrayAcredNac = Array.from(listaAcredNac);
-    const acredNac = arrayAcredNac.map((nodo) => nodo.innerText);
-
-    const widgetDiv = resDOM.body.querySelector(".copia-widget");
-    let widget;
-
-    if (widgetDiv) {
-      const widgetString = widgetDiv.querySelector("input").value;
-      const widgetBody = parser.parseFromString(widgetString, 'text/html').body;
-   
-      const anchorHref = widgetBody.querySelector("a").href;
-      const imgSrc = widgetBody.querySelector("img").src;
-      widget = {
-        anchorHref,
-        imgSrc,
+      let parser = new DOMParser();
+    
+      const resDOM = parser.parseFromString(res.html, 'text/html');
+      const errorP = resDOM.body.querySelector(".pull-left.help-block");
+      let contents;
+    
+      if (errorP) {
+        contents = {
+          error: errorP.innerText,
+        }
+      } else {
+        const journalTitle = resDOM.body.querySelector(".well-text-revista").querySelector("h1").innerText;
+        const journalURL = resDOM.body.querySelector(".table.table-striped").querySelector(".redibLink").href;
+        const redibURL = resDOM.body.querySelector(".dropdown").querySelector(".dropdown-menu").querySelector("a").href.split("?")[0];
+    
+        const listaNodoIndicadores = resDOM.body.querySelector(".indicadores").querySelectorAll(".indicador1");
+        const arrayNodos = Array.from(listaNodoIndicadores);
+        const indicadores = arrayNodos.map((nodo) => nodo.innerText);
+    
+        const listaAcredInter = resDOM.getElementsByClassName("acreditaciones")[0].querySelectorAll(".acreditacion1");
+        const arrayAcredInter = Array.from(listaAcredInter);
+        const acredInter = arrayAcredInter.map((nodo) => nodo.innerText);
+    
+        const listaAcredNac = resDOM.getElementsByClassName("acreditaciones")[1].querySelectorAll(".acreditacion1");
+        const arrayAcredNac = Array.from(listaAcredNac);
+        const acredNac = arrayAcredNac.map((nodo) => nodo.innerText);
+    
+        const widgetDiv = resDOM.body.querySelector(".copia-widget");
+        let widget;
+    
+        if (widgetDiv) {
+          const widgetString = widgetDiv.querySelector("input").value;
+          const widgetBody = parser.parseFromString(widgetString, 'text/html').body;
+      
+          const anchorHref = widgetBody.querySelector("a").href;
+          const imgSrc = widgetBody.querySelector("img").src;
+          widget = {
+            anchorHref,
+            imgSrc,
+          }
+        }
+        //La mayoría de los resultados no tienen esto
+        //const rankingDiv = resDOM.getElementById("rankingSerial");
+        contents = {
+          journalTitle,
+          journalURL,
+          redibURL,
+          indicadores,
+          acredInter,
+          acredNac,
+          widget
+        }
       }
-    }
-    //La mayoría de los resultados no tienen esto
-    //const rankingDiv = resDOM.getElementById("rankingSerial");
-    contents = {
-      journalTitle,
-      journalURL,
-      redibURL,
-      indicadores,
-      acredInter,
-      acredNac,
-      widget
-    }
+      return contents;
   }
-  return contents;
+
 }
