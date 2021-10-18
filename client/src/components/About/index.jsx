9b92eb4021b41606d34d0fb6201a4e1db5bcdaaf
Toto <toto_lp77@hotmail.com>
2021-10-18 09:18:44 -0300

    Componentes de FAQ y About

---
+++ client/src/components/About/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './about.css';
+
+
+// import instagramLogo from '../../images/insta.png';
+
+export default function About(){
+
+  return (
+    <div>
+      <p className="aboutP">
+      En la actualidad, los miembros de la comunidad académica y científica 
+      se enfrentan a una tarea ardua y engorrosa cuando deben determinar la calidad 
+      y el impacto de una publicación científica. 
+      Esta situación se debe a dos factores. 
+      En primer lugar, al crecimiento exponencial en la disponibilidad de recursos 
+      que tuvo el área debido al desarrollo tecnológico. 
+      En segundo lugar, la ausencia de estándares y la consecuente existencia de múltiples métricas y 
+      sistemas de evaluación que, aunque comparten objetivos, no siempre lo llevan a cabo de la misma manera.
+      </p>
+      <p className="aboutP">
+      <em>HERA</em> es una propuesta que busca dar respuesta al problema anterior 
+      y surge como una iniciativa conjunta entre diferentes unidades de la UNLP: la 
+      <a href="https://www.econo.unlp.edu.ar/investigacion_transferencia"> Secretaría de Investigación y Transferencia</a> de la Facultad de Ciencias Económicas, el 
+      <a href="http://weblidi.info.unlp.edu.ar/wp/"> III-LIDI</a> de la Facultad de Informática y el 
+      <a href="http://sedici.unlp.edu.ar/"> SEDICI</a>.
+      </p>
+      <p className="aboutP">
+      <em>HERA</em> es una herramienta que apunta a simplificar, 
+      agilizar y apoyar el proceso de determinar la calidad y el impacto de un recurso académico. 
+      Para ello integra  información proveniente de diferentes bases de datos académicas en forma rápida y concisa.
+
+      </p>
+
+    </div>
+  )
+}
---
+++ client/src/components/FAQ/index.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import './faq.css';
+
+
+// import instagramLogo from '../../images/insta.png';
+
+export default function FAQ(){
+
+  return (
+    <div>
+      <h1 className="faqQuestion">¿Qué es HERA?</h1>
+      <p className="faqP">
+        HERA es una herramienta que apunta a simplificar, agilizar 
+        y apoyar el proceso de determinar la calidad y el impacto de un recurso académico. 
+        Para ello integra  información proveniente de diferentes bases de datos académicas en forma rápida y concisa.
+      </p>
+
+      <h1 className="faqQuestion">¿Qué es un recurso académico?</h1>
+      <p className="faqP">
+        Utilizamos este término para referirnos genéricamente a dos clases de contenidos: 
+        Artículos de conferencias/revistas, capítulos de libros, o cualquier contenido que posea un DOI.
+        Revistas que posean un ISSN.
+      </p>
+
+      <h1 className="faqQuestion">¿Qué es un DOI?</h1>
+      <p className="faqP">
+        Document Object Identifier (DOI) es una cadena alfanumérica única para 
+        identificar contenido y proveer un vínculo persistente a su localización en Internet. 
+        Es habitual que las publicaciones científicas cuenten con esta clase de identificadores.
+      </p>
+
+      <h1 className="faqQuestion">¿Qué es un ISSN?</h1>
+      <p className="faqP">
+        El International Standard Serial Number (ISSN) es un código de 8 dígitos 
+        que sirve para identificar publicaciones periódicas y recursos continuos de toda clase 
+        y que sean editadas en cualquier soporte (en papel o digital).
+      </p>
+
+      <h1 className="faqQuestion">¿Qué información integra HERA?</h1>
+      <p className="faqP">
+        Dado un DOI o un ISSN, HERA obtiene información sobre la calidad 
+        y el impacto del recurso académico en cuestión. 
+        Para ello, consulta las bases de datos de <a href="https://www.crossref.org/">CrossRef</a>, <a href="https://doaj.org/">DOAJ</a>, <a href="https://academic.microsoft.com/">Microsoft Academic</a>, <a href="https://www.semanticscholar.org/">SemanticScholar</a>, 
+        <a href="https://redib.org/">REDIB</a>, <a href="https://mjl.clarivate.com/">Web of Science</a>, <a href="https://www.scopus.com/home.uri">Scopus</a>, <a href="https://www.scimagojr.com/">SJR</a>, <a href="https://www.dimensions.ai/">Dimensions</a> y <a href="https://www.altmetric.com/">Altmetric</a>.
+      </p>
+    </div>
+  )
+}
